bddfe6de11010c2069cf487f37ff1b227fa4a951
lzx21013048 <21013048@mail.ecust.edu.cn>
2024-06-07 20:59:42 +0800

    删除了不必要的文件，并整理了文件按结构

---
+++ others/templatetags/my_tags.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+from django import  template
+register = template.library
+
+
+=======
+from django import  template
+register = template.library
+
+
+>>>>>>> e8fe7138674da69fc28cd62c6c00a5b263f7e166
